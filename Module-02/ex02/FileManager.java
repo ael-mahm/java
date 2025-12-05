61457bd98d3a9dbb257be84db6a3a23af2af09d6
ael-mahm <aelmahmoudy1@gmail.com>
2025-12-05 15:50:51 +0100

    ex02

--- Module-02/ex02/FileManager.java
+++ Module-02/ex02/FileManager.java
@@ -22,10 +22,55 @@ public class FileManager {
             case "cd":
                 if (parts.length != 2)
                     throw new Exception("[x] cd FOLDER_NAME.");
+                break;
             default:
                 return null;
         }
         return parts;
+    }
+
+    public void mvCommand(String[] cmd) {
+        try {
+            File sr = new File(this.file, cmd[1]);
+            sr = sr.getCanonicalFile();
+            System.out.println(sr.getCanonicalFile());
+            if (!sr.exists() || !sr.isFile()) {
+                throw new Exception("File not found!");
+            }
+            File dst;
+            if (cmd[2].startsWith("/")) {
+                dst = new File(cmd[2]);
+            } else {
+                dst = new File(this.file, cmd[2]);
+            }
+            dst = dst.getCanonicalFile();
+            if (dst.exists() && dst.isDirectory()) {
+                dst = new File(dst, sr.getName());
+            } else {
+                dst = new File(this.file, cmd[2]).getCanonicalFile();
+            }
+            if (!(sr.renameTo(dst))) {
+                throw new Exception("Failed mv command");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void cdCommand(String[] cmd) {
+        File folder;
+
+        if (cmd[1].equals("..")) {
+            folder = this.file.getParentFile();
+        } else {
+            folder = new File(this.file, cmd[1]);
+        }
+
+        try {
+            folder = file.getCanonicalFile();
+            System.out.println(folder.getAbsolutePath());
+        } catch (Exception e) {
+        }
     }
 
     public void start() {
@@ -49,6 +94,12 @@ public class FileManager {
                     for (int i = 0; i < files.length; i++) {
                         System.out.println(files[i].getName() + " " + file.length() / 1024 + " KB");
                     }
+                }
+                if (command[0].equals("cd")) {
+                    cdCommand(command);
+                }
+                if (command[0].equals("mv")) {
+                    mvCommand(command);
                 }
             } catch (Exception e) {
                 System.out.println(e.getMessage());
